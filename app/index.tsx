d8f88f3e7cd6a1794c945e534710ec78579df10e
Bijoy Valappil <bijoythalakkalev@mashreq.com>
2024-01-26 23:50:44 +0400

    update push notification configs

--- app/index.tsx
+++ app/index.tsx
@@ -3,11 +3,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import AccessScreen from './screens/Access';
 import ProfileScreen from './screens/Profile';
+import registerNNPushToken from 'native-notify';
 import 'intl-pluralrules'
 
 const Stack = createStackNavigator();
 
 const App = () => {
+  registerNNPushToken(19079, 'f4jEGAy0H2W7Q3ZAl2LffM');
   return (
     <NavigationContainer independent={true}>
       <Stack.Navigator initialRouteName="Access" screenOptions={{
